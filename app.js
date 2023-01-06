e14d6604bc24c46ba936ce5612eeae84d0bc0433
niche <tlstndcjf@naver.com>
2023-01-06 20:23:08 +0900

    Create app.js
    update Calculator

---
+++ app.js
@@ -0,0 +1,11 @@
+const Calculator = {
+    add : function (x , y) {
+        console.log(x + y) ;
+    },
+    minus : function (x , y) {
+        console.log(x - y) ;
+    }
+};
+
+Calculator.add(10, 5)
+Calculator.minus(10, 5)
